5763a46c73c4be957e5b2902279a2ca02b66d19e
KirillArt <curillaenator@gmail.com>
2025-03-28 10:51:19 +0700

    fix: minor

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,23 +1,12 @@
 'use client';
 
 import React from 'react';
-import { ChakraProvider, createSystem, defaultConfig } from '@chakra-ui/react';
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
 import { ColorModeProvider, type ColorModeProviderProps } from './color-mode';
-
-const system = createSystem(defaultConfig, {
-  theme: {
-    tokens: {
-      fonts: {
-        heading: { value: `system-ui-rounded, ui-rounded, 'SanFrancisco', sans-serif` },
-        body: { value: `system-ui-rounded, ui-rounded, 'SanFrancisco', sans-serif` },
-      },
-    },
-  },
-});
 
 export function Provider(props: ColorModeProviderProps) {
   return (
-    <ChakraProvider value={system}>
+    <ChakraProvider value={defaultSystem}>
       <ColorModeProvider {...props} />
     </ChakraProvider>
   );
